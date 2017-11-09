feadf98a2d87047bbef384b9197467d4cf60e981
Rituj Beniwal <ritujbeniwal@gmail.com>
2017-11-09 17:31:07 +0530

    Screen stabiization
    Change-Id: Ibccb8d78d79938f42241ea3690ea66c3aace89ea

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -71,6 +71,7 @@ import com.android.server.connectivity.MetricsLoggerService;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.display.DisplayManagerService;
 import com.android.server.display.NightDisplayService;
+import com.android.server.display.ScreenStabilization;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.emergency.EmergencyAffordanceService;
 import com.android.server.fingerprint.FingerprintService;
@@ -1301,7 +1302,11 @@ public final class SystemServer {
                 Slog.i(TAG, "Starting PocketBridgeService");
                 mSystemServiceManager.startService(PocketBridgeService.class);
             }
-        }
+	}
+
+      	    traceBeginAndSlog("Starting ScreenStabilization Service");
+            mSystemServiceManager.startService(ScreenStabilization.class);
+            traceEnd();
 
         if (!disableNonCoreServices && !disableMediaProjection) {
             mSystemServiceManager.startService(MediaProjectionManagerService.class);
